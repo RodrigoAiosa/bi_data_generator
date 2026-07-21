4f351d34676308b21068d59dd7151007a11e4cb0
Rodrigo Aiosa <32776696+RodrigoAiosa@users.noreply.github.com>
2026-07-21 15:05:37 -0300

    Update __init__.py

--- generators/__init__.py
+++ generators/__init__.py
@@ -1,142 +1,151 @@
+"""
+generators/__init__.py
+Exporta todas as funções gerar_<setor> de cada módulo em generators/,
+além da bateria de medidas DAX (gerar_bateria_medidas).
+"""
+
+from .academia import gerar_academia
+from .agencia_publicidade import gerar_agencia_publicidade
 from .agtech import gerar_agtech
-from .economia_circular import gerar_economia_circular
-from .biotecnologia import gerar_biotecnologia
+from .agronegocio import gerar_agronegocio
+from .alimenticio import gerar_alimenticio
+from .apostas_esportivas import gerar_apostas_esportivas
 from .arquitetura import gerar_arquitetura
 from .audiovisual import gerar_audiovisual
+from .aviacao import gerar_aviacao
 from .beleza import gerar_beleza
+from .biotecnologia import gerar_biotecnologia
+from .ciberseguranca import gerar_ciberseguranca
 from .condominio import gerar_condominio
+from .construcao import gerar_construcao
+from .coworking import gerar_coworking
+from .crm import gerar_crm
+from .drones import gerar_drones
+from .economia_circular import gerar_economia_circular
+from .ecommerce import gerar_ecommerce
+from .editora import gerar_editora
+from .educacao import gerar_educacao
+from .energia import gerar_energia
+from .ensino_superior import gerar_ensino_superior
+from .espacial import gerar_espacial
+from .esportes import gerar_esportes
 from .eventos import gerar_eventos
+from .farmaceutico import gerar_farmaceutico
+from .financeiro import gerar_financeiro
+from .fintech import gerar_fintech
 from .florestal import gerar_florestal
 from .franquias import gerar_franquias
-from .laboratorio import gerar_laboratorio
-from .logistica_urbana import gerar_logistica_urbana
-from .moda import gerar_moda
-from .pesca import gerar_pesca
-from .saude_mental import gerar_saude_mental
-from .espacial import gerar_espacial
-from .startup import gerar_startup
-from .textil import gerar_textil
-from .viagem_corp import gerar_viagem_corp
-from .varejo import gerar_varejo
-from .financeiro import gerar_financeiro
-from .saude import gerar_saude
-from .tecnologia import gerar_tecnologia
-from .educacao import gerar_educacao
-from .logistica import gerar_logistica
-from .energia import gerar_energia
-from .telecom import gerar_telecom
-from .industria import gerar_industria
-from .agronegocio import gerar_agronegocio
-from .hotelaria import gerar_hotelaria
-from .streaming import gerar_streaming
-from .ecommerce import gerar_ecommerce
-from .rh import gerar_rh
-from .mobilidade import gerar_mobilidade
-from .fintech import gerar_fintech
-from .turismo import gerar_turismo
-from .imobiliario import gerar_imobiliario
-from .seguros import gerar_seguros
-from .construcao import gerar_construcao
-from .mineracao import gerar_mineracao
-from .alimenticio import gerar_alimenticio
-from .juridico import gerar_juridico
-from .esportes import gerar_esportes
-from .saas_b2b import gerar_saas_b2b
-from .crm import gerar_crm
-from .farmaceutico import gerar_farmaceutico
-from .marketing import gerar_marketing
-from .petroleo import gerar_petroleo
-from .governo import gerar_governo
-from .portabilidade_claro import gerar_portabilidade_claro
-from .aviacao import gerar_aviacao
-from .pet import gerar_pet
+from .frigorifico import gerar_frigorifico
+from .funeraria import gerar_funeraria
 from .games import gerar_games
-from .saneamento import gerar_saneamento
-from .transporte import gerar_transporte
-from .odontologia import gerar_odontologia
-from .academia import gerar_academia
-from .ciberseguranca import gerar_ciberseguranca
+from .governo import gerar_governo
+from .homecare import gerar_homecare
+from .hotelaria import gerar_hotelaria
+from .imobiliario import gerar_imobiliario
+from .industria import gerar_industria
+from .juridico import gerar_juridico
+from .laboratorio import gerar_laboratorio
 from .locadora import gerar_locadora
+from .logistica import gerar_logistica
+from .logistica_urbana import gerar_logistica_urbana
+from .marketing import gerar_marketing
+from .portabilidade_claro import gerar_portabilidade_claro
+from .mineracao import gerar_mineracao
+from .mobilidade import gerar_mobilidade
+from .moda import gerar_moda
+from .odontologia import gerar_odontologia
+from .pecuaria import gerar_pecuaria
+from .pesca import gerar_pesca
+from .pet import gerar_pet
+from .petroleo import gerar_petroleo
+from .rh import gerar_rh
 from .restaurante import gerar_restaurante
+from .saas_b2b import gerar_saas_b2b
+from .saneamento import gerar_saneamento
+from .saude import gerar_saude
+from .saude_mental import gerar_saude_mental
+from .seguros import gerar_seguros
+from .startup import gerar_startup
+from .streaming import gerar_streaming
+from .tecnologia import gerar_tecnologia
+from .telecom import gerar_telecom
+from .textil import gerar_textil
+from .transporte import gerar_transporte
+from .turismo import gerar_turismo
+from .varejo import gerar_varejo
+from .viagem_corp import gerar_viagem_corp
 from .medidas import gerar_bateria_medidas
 
-from .moda import gerar_moda
-from .eventos import gerar_eventos
-from .laboratorio import gerar_laboratorio
-from .franquias import gerar_franquias
-from .condominio import gerar_condominio
-from .saude_mental import gerar_saude_mental
-from .florestal import gerar_florestal
-from .startup import gerar_startup
-from .audiovisual import gerar_audiovisual
-from .pesca import gerar_pesca
-from .textil import gerar_textil
-from .arquitetura import gerar_arquitetura
-from .viagem_corp import gerar_viagem_corp
-from .espacial import gerar_espacial
-from .beleza import gerar_beleza
-from .logistica_urbana import gerar_logistica_urbana
-
 __all__ = [
-    "gerar_varejo",
-    "gerar_financeiro",
-    "gerar_saude",
-    "gerar_tecnologia",
-    "gerar_educacao",
-    "gerar_logistica",
-    "gerar_energia",
-    "gerar_telecom",
-    "gerar_industria",
-    "gerar_agronegocio",
-    "gerar_hotelaria",
-    "gerar_streaming",
-    "gerar_ecommerce",
-    "gerar_rh",
-    "gerar_mobilidade",
-    "gerar_fintech",
-    "gerar_turismo",
-    "gerar_imobiliario",
-    "gerar_seguros",
-    "gerar_construcao",
-    "gerar_mineracao",
-    "gerar_alimenticio",
-    "gerar_juridico",
-    "gerar_esportes",
-    "gerar_saas_b2b",
-    "gerar_crm",
-    "gerar_farmaceutico",
-    "gerar_marketing",
-    "gerar_petroleo",
-    "gerar_governo",
-    "gerar_portabilidade_claro",
-    "gerar_aviacao",
-    "gerar_pet",
-    "gerar_games",
-    "gerar_saneamento",
-    "gerar_transporte",
-    "gerar_moda",
-    "gerar_eventos",
-    "gerar_laboratorio",
-    "gerar_franquias",
-    "gerar_condominio",
-    "gerar_saude_mental",
-    "gerar_florestal",
-    "gerar_startup",
-    "gerar_audiovisual",
-    "gerar_pesca",
-    "gerar_textil",
-    "gerar_arquitetura",
-    "gerar_viagem_corp",
-    "gerar_espacial",
-    "gerar_beleza",
-    "gerar_logistica_urbana",
-    "gerar_agtech",
-    "gerar_economia_circular",
-    "gerar_biotecnologia",
-    "gerar_odontologia",
     "gerar_academia",
+    "gerar_agencia_publicidade",
+    "gerar_agtech",
+    "gerar_agronegocio",
+    "gerar_alimenticio",
+    "gerar_apostas_esportivas",
+    "gerar_arquitetura",
+    "gerar_audiovisual",
+    "gerar_aviacao",
+    "gerar_beleza",
+    "gerar_biotecnologia",
     "gerar_ciberseguranca",
+    "gerar_condominio",
+    "gerar_construcao",
+    "gerar_coworking",
+    "gerar_crm",
+    "gerar_drones",
+    "gerar_economia_circular",
+    "gerar_ecommerce",
+    "gerar_editora",
+    "gerar_educacao",
+    "gerar_energia",
+    "gerar_ensino_superior",
+    "gerar_espacial",
+    "gerar_esportes",
+    "gerar_eventos",
+    "gerar_farmaceutico",
+    "gerar_financeiro",
+    "gerar_fintech",
+    "gerar_florestal",
+    "gerar_franquias",
+    "gerar_frigorifico",
+    "gerar_funeraria",
+    "gerar_games",
+    "gerar_governo",
+    "gerar_homecare",
+    "gerar_hotelaria",
+    "gerar_imobiliario",
+    "gerar_industria",
+    "gerar_juridico",
+    "gerar_laboratorio",
     "gerar_locadora",
+    "gerar_logistica",
+    "gerar_logistica_urbana",
+    "gerar_marketing",
+    "gerar_portabilidade_claro",
+    "gerar_mineracao",
+    "gerar_mobilidade",
+    "gerar_moda",
+    "gerar_odontologia",
+    "gerar_pecuaria",
+    "gerar_pesca",
+    "gerar_pet",
+    "gerar_petroleo",
+    "gerar_rh",
     "gerar_restaurante",
+    "gerar_saas_b2b",
+    "gerar_saneamento",
+    "gerar_saude",
+    "gerar_saude_mental",
+    "gerar_seguros",
+    "gerar_startup",
+    "gerar_streaming",
+    "gerar_tecnologia",
+    "gerar_telecom",
+    "gerar_textil",
+    "gerar_transporte",
+    "gerar_turismo",
+    "gerar_varejo",
+    "gerar_viagem_corp",
     "gerar_bateria_medidas",
 ]
